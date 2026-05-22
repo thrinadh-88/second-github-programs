bcf7ed1efe21b7977e72e049b14d5b0e09b09618
thrinadh-88 <tkadimi8@gmail.com>
2026-05-22 14:54:46 +0530

    basics of python

---
+++ basics.py
@@ -0,0 +1,29 @@
+#Arithmatic operators
+a = 10
+b = 20
+print(a+b) #Addition
+print(a-b) #Subtraction
+print(a*b) #Multiplication
+print(a/b) #Division
+print(a//b) #Floor division
+print(a%b) #Modulus
+#Comparison operators
+print(a==b) #Equal to
+print(a!=b) #Not equal to
+print(a>b) #Greater than
+print(a<b) #Less than
+print(a>=b) #Greater than or equal to
+print(a<=b) #Less than or equal to
+print(a is b) #Identity operator
+print(a is not b) #Not identity operator
+#Logical operators
+print(a > 5 and b > 15) #Logical AND
+print(a > 5 or b > 15) #Logical OR
+print(not(a > 5 and b > 15)) #Logical NOT
+#Bitwise operators
+print(a & b) #Bitwise AND
+print(a | b) #Bitwise OR
+print(a ^ b) #Bitwise XOR
+print(~a) #Bitwise NOT
+print(a << 2) #Bitwise left shift
+print(a >> 2) #Bitwise right shift
