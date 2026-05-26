05d3f7fe77472bbe38ffb5bdddc14e55f7772678
thrinadh-88 <tkadimi8@gmail.com>
2026-05-26 10:08:52 +0530

    while loops

---
+++ loops.py
@@ -0,0 +1,16 @@
+#name = input("Enter your name: ")
+#while name == "":
+#    print("Name cannot be empty. Please enter your name.")
+#    name = input("Enter your name: ")
+#print(f"Hello {name}!")
+
+#age = int(input("Enter your age:"))
+
+#while age < 0:
+    #print("age cannot be negative")
+   # age = int(input("Enter your age:"))
+
+#if age <= 18:
+  #  print(f"{name} is not eligible to vote")
+#else:
+ #   print(f"{name} is eligible to vote")
